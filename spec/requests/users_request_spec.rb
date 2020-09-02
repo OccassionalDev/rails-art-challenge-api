6eb4949184d462856df293a7c5b42682f57697ef
OccassionalDev <ericaschimmel01@gmail.com>
2020-09-01 22:01:30 -0400

    added documentation formating to .rpsec and added a test that checks index request for users to contain a list of users

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+    describe "GET users#index" do
+        it "should get a list of users with valid attributes" do 
+            get "/api/v1/users"
 
+            user = User.first
+            json = JSON.parse(response.body)
+
+            expect(response).to have_http_status(200)
+            expect(json).to contain_exactly({
+                id: user.id,
+                email: user.email,
+                username: user.username
+            }.stringify_keys)
+        end 
+    end 
 end
