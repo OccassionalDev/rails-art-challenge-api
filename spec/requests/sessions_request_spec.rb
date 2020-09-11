13d77bfd99124f9c8e3b6e6b92835e81ba1640ff
OccassionalDev <ericaschimmel01@gmail.com>
2020-09-10 23:12:51 -0400

    created a create action in sessions controller and a empty test in sessions_request_spec

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-
+    describe "POST sessions#create" do 
+        it "should succesfully log a valid user in" do 
+        end 
+    end 
 end
