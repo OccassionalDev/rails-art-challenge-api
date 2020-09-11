d9fba790271bb129f16e3a5acf1ccc604ea2fd67
OccassionalDev <ericaschimmel01@gmail.com>
2020-09-10 22:44:56 -0400

    added to the should create test to make sure no errors appear, and added a extra test to make sure invalid attributes prevent a user from being created in user_request_spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -23,6 +23,21 @@ RSpec.describe "Users", type: :request do
             
             post "/api/v1/users", :params => new_user.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
             expect(response).to have_http_status(200)
+
+            json = JSON.parse(response.body)
+
+            expect(json["errors"]).to be_nil
+        end 
+
+        it "should not create a new user with invalid attributes" do 
+            new_user = { user: { username: "test_user", email: "test@test.com", password: "asdf", password_confirmation: "asdfgh" } }
+            
+            post "/api/v1/users", :params => new_user.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
+            expect(response).to have_http_status(200)
+
+            json = JSON.parse(response.body)
+            
+            expect(json["errors"]).to_not be_nil
         end 
     end
 end
