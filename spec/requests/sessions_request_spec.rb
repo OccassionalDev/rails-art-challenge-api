28a768bdda165d7d0bc69555b92bdb06d5b6e158
OccassionalDev <ericaschimmel01@gmail.com>
2020-09-21 17:53:00 -0400

    test for logging a user in through sessions_request_spec.rb is functional

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
     describe "POST sessions#create" do 
-        it "should succesfully log a valid user in" do 
+        it "should succesfully log a valid user in" do
+            user = User.first
+            login_params = { email: "cr@example.com", password: "sample" }
+
+            post "/api/v1/login", :params => login_params.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
+            json = JSON.parse(response.body) 
+
+            expect(response).to have_http_status(200)
+            expect(json["errors"]).to be_nil
         end 
     end 
 end
