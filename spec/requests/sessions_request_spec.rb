d2c9be06de07a49be82c94212c6f5871664fafc0
OccassionalDev <ericaschimmel01@gmail.com>
2020-09-21 18:08:47 -0400

    added in two more tests to test a invalid login and a session cookie creation upon logging in succesfully

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "Sessions", type: :request do
     describe "POST sessions#create" do 
         it "should succesfully log a valid user in" do
-            user = User.first
             login_params = { email: "cr@example.com", password: "sample" }
 
             post "/api/v1/login", :params => login_params.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
@@ -11,6 +10,25 @@ RSpec.describe "Sessions", type: :request do
 
             expect(response).to have_http_status(200)
             expect(json["errors"]).to be_nil
+        end 
+
+        it "should not log a invalid user in" do 
+            invalid_params = { email: "asdf", password: "asdf" }
+
+            post "/api/v1/login", :params => invalid_params.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
+            json = JSON.parse(response.body) 
+
+            expect(response).to have_http_status(200)
+            expect(json["errors"]).to_not be_nil
+        end
+
+        it "should create a session cookie upon logging in" do 
+            login_params = { email: "cr@example.com", password: "sample" }
+
+            post "/api/v1/login", :params => login_params.to_json,  :headers => { "CONTENT_TYPE" => "application/json" }
+
+            expect(response).to have_http_status(200)
+            expect(session[:user_id]).to_not be_nil
         end 
     end 
 end
